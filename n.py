5c906b4864eb605696293450189b376f26ad92ed
beastamrit <mainamritjha@gmail.com>
2024-12-08 20:38:28 +0530

    bitplay

---
+++ n.py
@@ -0,0 +1,8 @@
+def abc(a,b):
+    if ((a^b)!=0):
+        print("Number are not equal")
+    else:
+        print("Number are  equal")
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+abc(a,b)
